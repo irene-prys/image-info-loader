827f9b9d0adec9bc00ac44433b42235f3914a5b6
irene <irene.prys@gmail.com>
2017-09-24 17:19:00 +0300

    moved resources to the proper place

--- src/main/java/demo/image/loader/controllers/ImageInfoController.java
+++ src/main/java/demo/image/loader/controllers/ImageInfoController.java
@@ -2,26 +2,19 @@ package demo.image.loader.controllers;
 
 import demo.image.loader.services.ImageInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 public class ImageInfoController {
 
     @Autowired
     private ImageInfoService imageLoaderService;
 
     @RequestMapping("/load-info")
-    @ResponseBody
     public String loadInfo() throws IOException {
         return imageLoaderService.loadImageInfo();
-    }
-
-    @RequestMapping({"/image", "/"})
-    public String index() {
-        return "index";
     }
 }
